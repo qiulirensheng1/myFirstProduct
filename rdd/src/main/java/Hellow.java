0df3396a410a0921b53aa12b5ecbd02783daf5e0
raodongdong <35055601+raodongdong@users.noreply.github.com>
2018-10-02 12:21:44 +0800

    增加了555

--- rdd/src/main/java/Hellow.java
+++ rdd/src/main/java/Hellow.java
@@ -6,7 +6,10 @@ public class Hellow {
     public static void main(String[] args) {
         System.out.println("hello world...");
         System.out.println("333 = " + 333);
+        System.out.println("555"+555);
+
         System.out.println("Hellow.main"+444);
+
     }
 
 }
